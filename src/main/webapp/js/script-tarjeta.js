ef944dd60c72582c7c2c2b797d555fb12da7defd
JuniorMolina23 <junior.molina@tecsup.edu.pe>
2022-08-26 16:40:55 -0500

    Lab01

---
+++ src/main/webapp/js/script-tarjeta.js
@@ -0,0 +1,7 @@
+
+for (let index = 0; index < imagenes.length; index++) {
+    const div = document.createElement('div')
+    div.id = index
+    div.classList.add('circulos')
+    divIndicadores.appendChild(div)
+}
